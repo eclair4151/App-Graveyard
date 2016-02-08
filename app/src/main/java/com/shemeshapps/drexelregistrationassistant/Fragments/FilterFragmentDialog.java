2f0cc3cc3804805d744b7fd9e9c1379c81e06f03
Tomer Shemesh <tomerjshemesh@gmail.com>
2016-02-08 10:19:46 -0500

    added popup to select profs

--- app/src/main/java/com/shemeshapps/drexelregistrationassistant/Fragments/FilterFragmentDialog.java
+++ app/src/main/java/com/shemeshapps/drexelregistrationassistant/Fragments/FilterFragmentDialog.java
@@ -2,10 +2,13 @@ package com.shemeshapps.drexelregistrationassistant.Fragments;
 
 import android.app.DialogFragment;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.shemeshapps.drexelregistrationassistant.R;
 
@@ -21,9 +24,44 @@ public class FilterFragmentDialog extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.filter_popup_fragment, container);
+        View parentView = inflater.inflate(R.layout.filter_popup_fragment, container);
         getDialog().setTitle("Filter");
+        Button b = (Button)parentView.findViewById(R.id.filter_by_prof_button);
+        final String profs[] = {"Erin Solovey","David Breen","William Mongan"};
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                // Set the dialog title
+                builder.setTitle("Select Professors")
+                        // Specify the list array, the items to be selected by default (null for none),
+                        // and the listener through which to receive callbacks when items are selected
+                        .setMultiChoiceItems(profs, null,
+                                new DialogInterface.OnMultiChoiceClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which,
+                                                        boolean isChecked) {
 
-        return view;
+                                    }
+                                })
+                        // Set the action buttons
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User clicked OK, so save the mSelectedItems results somewhere
+                                // or return them to the component that opened the dialog
+
+                            }
+                        })
+                        .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                builder.create().show();
+            }
+        });
+        return parentView;
     }
 }
